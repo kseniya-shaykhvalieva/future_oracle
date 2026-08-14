97f5a36a6986efa6f8f0b2c5235df0ef00361d8a
kseniya-shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-08-14 17:43:00 +0500

    Интерфейс прогнозов

--- predictions/urls.py
+++ predictions/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from predictions.apps import PredictionsConfig
-from predictions.views import MatchPredictionView, MatchListView
+from predictions.views import MatchPredictionView, MatchListAPIView, MatchListView, MatchDetailView
 
 app_name = PredictionsConfig.name
 
 urlpatterns = [
     path('predict/<int:match_id>/', MatchPredictionView.as_view(), name='predict'),
-    path('matches/', MatchListView.as_view(), name='matches'),
+    path('matches/', MatchListAPIView.as_view(), name='matches'),
+    path('', MatchListView.as_view(), name='match_list'),
+    path('match/<int:pk>/', MatchDetailView.as_view(), name='match_detail'),
 ]
--- predictions/views.py
+++ predictions/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import TemplateView, DetailView
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.response import Response
 from .models import Match
@@ -15,6 +16,26 @@ class MatchPredictionView(RetrieveAPIView):
         return Response(result)
 
 
-class MatchListView(ListAPIView):
+class MatchListAPIView(ListAPIView):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
+
+
+class MatchListView(TemplateView):
+    template_name = 'predictions/matches.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['matches'] = Match.objects.all()[:10]
+        return context
+
+
+class MatchDetailView(DetailView):
+    model = Match
+    template_name = 'predictions/match_detail.html'
+    context_object_name = 'match'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['prediction'] = get_prediction(self.object.match_id)
+        return context
